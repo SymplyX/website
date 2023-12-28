9a258e32a16e29a180862225665d5fa0ca78e0d6
AzaleeX <azaleemcpe@gmail.com>
2023-12-28 11:56:57 +0100

    🎯Fix Responsive Website

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,33 +1,27 @@
-import {Navbar} from "@/components/navbar";
-import {Head} from "./head";
+import { Navbar } from "@/components/navbar";
+import { Head } from "./head";
 import Footer from "@/components/footer";
-import {Image, Spacer} from "@nextui-org/react";
+import { Image, Spacer } from "@nextui-org/react";
 import React from "react";
 
 export default function DefaultLayout({
-	children,
-}: {
+										  children,
+									  }: {
 	children: React.ReactNode;
 }) {
 	return (
 		<div className="relative flex flex-col min-h-screen overflow-hidden">
-			<div className="absolute inset-0 overflow-hidden opacity-50">
-				<Image
-					src="/hero.png"
-					alt="Hero Background"
-					className="object-cover object-center w-full h-full md:object-contain md:object-center"
-				/>
-			</div>
-
-			<div className="relative z-10 flex-grow overflow-y-auto">
-				<Head/>
-				<Navbar/>
-				<main className="container mx-auto max-w-7xl px-6">
+			<div className="relative z-10 flex-grow overflow-y-auto h-full">
+				<div className="absolute inset-0 bg-black opacity-50"></div>
+				<div className="absolute inset-0 opacity-50" style={{ backgroundImage: 'url(/hero.png)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+				<Head />
+				<Navbar />
+				<main className="container mx-auto px-4 sm:px-6 lg:px-8">
 					{children}
 				</main>
-				<Spacer y={96}/>
-				<Footer/>
+				<Spacer y={ 96 } />
 			</div>
+			<Footer />
 		</div>
 	);
 }
